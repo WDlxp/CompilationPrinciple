7b3f973aed36c92de25dd6f20ac1d3c36a9a3696
WDL <18959240295@163.com>
2019-04-02 00:01:30 +0800

    输入正规式和单词判断是否合法

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,108 @@
+/**
+ * @author wdl
+ */
 public class Main {
-
     public static void main(String[] args) {
-        String string = "(((a b|b )*(ab|c.d))*)*";
+        /* (((i n|t )*(ab|c.d))*)* */
+        String string = "int";
+        String input = "int";
+        boolean print = true;
+        System.out.println("合法性判断结果：" + isLegitimacy(string, input, print));
+    }
+
+    /**
+     * 合法性判断
+     *
+     * @param string 正规式
+     * @param input  判断的单词
+     * @param print  是否打印过程
+     * @return 返回是否合法
+     */
+    private static boolean isLegitimacy(String string, String input, boolean print) {
+        if (returnMiniDFA(string, print).isTrue()) {
+            int[][] miniDFA = returnMiniDFA(string, print).getMiniDFA();
+            SuffixToNFA.NFA nfa = null;
+            StringBuilder result = new StringBuilder();
+            InfixToSuffix.change(string, result);
+            nfa = SuffixToNFA.changeSuffixToNfa(result.toString());
+            return legitimacy(miniDFA, input, nfa);
+        }
+        return false;
+    }
+
+    /**
+     * returnMiniDFA返回值的数据结构
+     */
+    static class Result {
+        private int[][] miniDFA;
+        private boolean isTrue;
+
+        int[][] getMiniDFA() {
+            return miniDFA;
+        }
+
+        void setMiniDFA(int[][] miniDFA) {
+            this.miniDFA = miniDFA;
+        }
+
+        boolean isTrue() {
+            return isTrue;
+        }
+
+        void setTrue(boolean aTrue) {
+            isTrue = aTrue;
+        }
+    }
+
+    /**
+     * @param string 正规式
+     * @return Result 返回miniDFA和正规式是否正确
+     */
+    private static Result returnMiniDFA(String string, boolean print) {
         StringBuilder result = new StringBuilder();
-        System.out.println("正规式为：" + string);
 
         SuffixToNFA.NFA nfa = null;
-
+        Result result1 = new Result();
         if (InfixToSuffix.change(string, result) == 0) {
-            System.out.println("后缀表达式：" + result.toString());
             nfa = SuffixToNFA.changeSuffixToNfa(result.toString());
-            SuffixToNFA.printNFA(nfa);
-            /* 返回一个最小DFA的二维矩阵，横坐标对应字符集的顺序，最后列代表是否为终态1代表是0代表不是，同时第一行即状态0代表入口 */
-            int[][] minDFA = NFAToDFA.changeNFAToDFA(nfa, true);
+            if (print) {
+                SuffixToNFA.printNFA(nfa);
+            }
+
+            result1.setMiniDFA(NFAToDFA.changeNFAToDFA(nfa, print));
+            result1.setTrue(true);
+
         } else {
-            System.out.println("输入有误");
+
+            result1.setTrue(false);
         }
+        return result1;
+    }
+
+
+    private static boolean legitimacy(int[][] miniDFA, String input, SuffixToNFA.NFA nfa) {
+        int i = -1;
+        char[] characters = new char[nfa.getCharacterSet().size()];
+        for (char ch : nfa.getCharacterSet()) {
+            characters[++i] = ch;
+        }
+        int current = 0;
+        for (int j = 0; j < input.length(); j++) {
+            char op = input.charAt(j);
+            for (int index = 0; index < nfa.getCharacterSet().size(); index++) {
+                if (characters[index] == op) {
+                    if (miniDFA[current][index] == -1) {
+                        if (j == input.length() - 1) {
+                            return miniDFA[current][characters.length] == 1;
+                        }
+                        return false;
+                    } else {
+                        current = miniDFA[current][index];
+                        break;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
