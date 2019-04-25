0c6584487402b077891c0539a4653e26f7eac559
WDL <18959240295@163.com>
2019-04-25 13:15:59 +0800

    修改将字符集集中

--- src/lexicalAnalysis/LexicalAnalysis.java
+++ src/lexicalAnalysis/LexicalAnalysis.java
@@ -18,8 +18,109 @@ class LexicalAnalysis {
      */
     static void lexicalAnalysis(String filePath, String regularFormFilePath, String resultFilePath, boolean isPrint) throws IOException {
 
-        //关键字集合
-        HashSet keyWordSet = new HashSet() {{
+
+        //读取正规式文件和单词文件
+        String readWordsFileString = readFile(filePath, " ");
+        String readRegularFormString = readFile(regularFormFilePath, "\n");
+        //分割字符,预处理文件内容
+        String readWordsFileString1 = operatorAddSpace(readWordsFileString);
+        System.out.println("文件内容：" + readWordsFileString);
+        System.out.println("处理后文件内容：" + readWordsFileString1);
+
+        //放置正规式返回的miniDFA的集合
+        ArrayList<Result> miniDfaResultList = new ArrayList<>();
+        //放置正规式集合
+        ArrayList<String> regularFormList = new ArrayList<>();
+        //将读取出来的正规式按行划分
+        String[] regularFormStrings = readRegularFormString.split("\n");
+        for (String regularFormString : regularFormStrings) {
+            //划分出的取出的非空格才是需要处理的正规式
+            if (!regularFormString.equals("")) {
+                regularFormList.add(regularFormString);
+                Result result = returnMiniDFA(regularFormString, isPrint);
+                miniDfaResultList.add(result);
+                if (!result.isTrue()) {
+                    System.out.println("正规式" + regularFormString + "输入有误");
+                }
+            }
+        }
+        //获取miniDFA的结果集合长度
+        int len = miniDfaResultList.size();
+        if (len != 0) {
+            // 构建FileOutputStream对象,文件不存在会自动新建
+            FileOutputStream fop = new FileOutputStream(resultFilePath);
+            // 构建OutputStreamWriter对象,参数可以指定编码,默认为操作系统默认编码,windows上是gbk
+            OutputStreamWriter writer = new OutputStreamWriter(fop, "UTF-8");
+
+            // 构建FileOutputStream对象,文件不存在会自动新建
+            int[][] miniDFA = null;
+            char[] characters = null;
+
+            if (!readWordsFileString.isEmpty()) {
+                String[] division = readWordsFileString1.split(" ");
+                for (String cell : division) {
+                    if (!cell.equals("")) {
+                        boolean isRight = false;
+                        System.out.println(cell);
+                        if (SymbolSets.KEY_WORD_SET.contains(cell)) {
+                            writer.append(cell + "\n合法性：" + true + " Java关键字");
+                            System.out.println("合法性：" + true + " Java关键字");
+                        } else if (SymbolSets.RESERVED_WORD_SET.contains(cell)) {
+                            writer.append(cell + "\n" + true + " Java保留字");
+                            System.out.println("合法性： " + true + " Java保留字");
+                        } else if (SymbolSets.FLAG_SET.contains(cell)) {
+                            writer.append(cell + "\n " + true + " Java标志符");
+                            System.out.println("合法性： " + true + " Java标志符");
+                        } else if (SymbolSets.OPERATOR_SET.contains(cell)) {
+                            writer.append(cell + "\n " + true + " Java运算符");
+                            System.out.println("合法性： " + true + " Java运算符");
+                        } else if (SymbolSets.SPLITTER_SET.contains(cell)) {
+                            writer.append(cell + "\n " + true + " Java分割符");
+                            System.out.println("合法性： " + true + " Java分割符");
+                        } else {
+                            int i = 0;
+                            for (; i < len; i++) {
+                                Result result = miniDfaResultList.get(i);
+                                if (result.isTrue()) {
+                                    miniDFA = result.getMiniDFA();
+                                    characters = result.getCharacters();
+                                    isRight = legitimacy(miniDFA, cell, characters);
+                                }
+                                if (isRight) {
+                                    break;
+                                }
+                            }
+                            if (isRight) {
+                                // 写入到缓冲区
+                                writer.append(cell + "\n合法性： " + isRight + " 符合第" + (i + 1) + "个正规式: " + regularFormList.get(i));
+                                System.out.println("合法性： " + isRight + "  符合第" + (i + 1) + "个正规式: " + regularFormList.get(i));
+                            } else {
+                                writer.append(cell + "\n合法性： " + isRight);
+                                System.out.println("合法性： " + isRight);
+                            }
+                        }
+                        // 换行
+                        writer.append("\r\n");
+                    }
+                }
+            } else {
+                System.out.println("文件内容为空！");
+            }
+            // 刷新缓存冲,写入到文件,如果下面已经没有写入的内容了,直接close也会写入
+            writer.close();
+        } else {
+            System.out.println("请在正规式文件中输入正规式以行划分");
+        }
+    }
+
+    /**
+     * 字符集合的集合
+     */
+    static class SymbolSets {
+        /**
+         * 关键字集合
+         */
+        static final HashSet KEY_WORD_SET = new HashSet() {{
             add("abstract");
             add("assert");
             add("boolean");
@@ -69,104 +170,46 @@ class LexicalAnalysis {
             add("volatile");
             add("while");
         }};
-        //标志符集合
-        HashSet reservedwordset = new HashSet(){{
+        /**
+         * 标志符集合
+         */
+        static final HashSet RESERVED_WORD_SET = new HashSet() {{
             add("goto");
             add("const");
         }};
-        //标志符集合
-        HashSet flagSet = new HashSet(){{
+        /**
+         * 标志符集合
+         */
+        static final HashSet FLAG_SET = new HashSet() {{
             add("null");
             add("true");
             add("false");
         }};
+        /**
+         * 运算符集合
+         */
+        static final HashSet OPERATOR_SET = new HashSet() {{
+            add("=");
+            add("+");
+            add("-");
+            add("<");
+            add(">");
+            add("/");
+            add("(");
+            add(")");
+            add("{");
+            add("}");
+            add("!");
+        }};
 
-        //读取正规式文件和单词文件
-        String readWordsFileString = readFile(filePath, " ");
-        String readRegularFormString = readFile(regularFormFilePath, "\n");
-        //分割字符,预处理文件内容
-        String readWordsFileString1 = chuli(readWordsFileString);
-        System.out.println("文件内容："+ readWordsFileString);
-        System.out.println("处理后文件内容："+readWordsFileString1);
-
-        //放置正规式返回的miniDFA的集合
-        ArrayList<Result> miniDfaResultList = new ArrayList<>();
-        //放置正规式集合
-        ArrayList<String> regularFormList = new ArrayList<>();
-        //将读取出来的正规式按行划分
-        String[] regularFormStrings = readRegularFormString.split("\n");
-        for (String regularFormString : regularFormStrings) {
-            //划分出的取出的非空格才是需要处理的正规式
-            if (!regularFormString.equals("")) {
-                regularFormList.add(regularFormString);
-                Result result = returnMiniDFA(regularFormString, isPrint);
-                miniDfaResultList.add(result);
-                if (!result.isTrue()) {
-                    System.out.println("正规式" + regularFormString + "输入有误");
-                }
-            }
-        }
-        //获取miniDFA的结果集合长度
-        int len = miniDfaResultList.size();
-        if (len != 0) {
-            // 构建FileOutputStream对象,文件不存在会自动新建
-            FileOutputStream fop = new FileOutputStream(resultFilePath);
-            // 构建OutputStreamWriter对象,参数可以指定编码,默认为操作系统默认编码,windows上是gbk
-            OutputStreamWriter writer = new OutputStreamWriter(fop, "UTF-8");
-
-            // 构建FileOutputStream对象,文件不存在会自动新建
-            int[][] miniDFA = null;
-            char[] characters = null;
-
-            if (!readWordsFileString.isEmpty()) {
-                String[] division = readWordsFileString1.split(" ");
-                for (String cell : division) {
-                    if (!cell.equals("")) {
-                        boolean isRight = false;
-                        System.out.println(cell);
-                        if (keyWordSet.contains(cell)){
-                            writer.append(cell + "\n合法性：" + true + " Java关键字");
-                            System.out.println("合法性：" + true +  " Java关键字");
-                        }else if (reservedwordset.contains(cell)){
-                            writer.append(cell + "\n" + true + " Java保留字");
-                            System.out.println("合法性： " + true +  " Java保留字");
-                        }else if (flagSet.contains(cell)){
-                            writer.append(cell + "\n " + true + " Java标志符");
-                            System.out.println("合法性： " + true +  " Java标志符");
-                        }else {
-                            int i = 0;
-                            for (; i < len; i++) {
-                                Result result = miniDfaResultList.get(i);
-                                if (result.isTrue()) {
-                                    miniDFA = result.getMiniDFA();
-                                    characters = result.getCharacters();
-                                    isRight = legitimacy(miniDFA, cell, characters);
-                                }
-                                if (isRight) {
-                                    break;
-                                }
-                            }
-                            if (isRight) {
-                                // 写入到缓冲区
-                                writer.append(cell + "\n合法性： " + isRight + " 符合第" + (i + 1) + "个正规式: " + regularFormList.get(i));
-                                System.out.println("合法性： " + isRight + "  符合第" + (i + 1) + "个正规式: " + regularFormList.get(i));
-                            } else {
-                                writer.append(cell + "\n合法性： " + isRight);
-                                System.out.println("合法性： " + isRight);
-                            }
-                        }
-                        // 换行
-                        writer.append("\r\n");
-                    }
-                }
-            } else {
-                System.out.println("文件内容为空！");
-            }
-            // 刷新缓存冲,写入到文件,如果下面已经没有写入的内容了,直接close也会写入
-            writer.close();
-        } else {
-            System.out.println("请在正规式文件中输入正规式以行划分");
-        }
+        /**
+         * 分割符集合
+         */
+        static final HashSet SPLITTER_SET = new HashSet() {{
+            add(";");
+            add(":");
+            add("|");
+        }};
     }
 
     /**
@@ -287,43 +330,31 @@ class LexicalAnalysis {
         }
         return miniDFA[current][characters.length] == 1;
     }
-    private static String chuli(String string) {
-        HashSet yunSuanFuSet = new HashSet() {{
-            add("=");
-            add("+");
-            add("-");
-            add("<");
-            add(">");
-            add("/");
-            add("(");
-            add(")");
-            add("{");
-            add("}");
-        }};
-        HashSet fenGeFuSet = new HashSet() {{
-            add(";");
-            add(":");
-            add("|");
-            add("1");
-        }};
+
+    /**
+     * 运算符两边加上空格辅助后续分割
+     * @param string 传入要处理的字符串
+     * @return 返回处理后的字符串
+     */
+    private static String operatorAddSpace(String string) {
         StringBuilder stringBuilder = new StringBuilder();
-        boolean isyunsuanfu = true;
+        boolean isOperator = true;
         StringBuilder same = new StringBuilder();
         for (int i = 0; i < string.length(); i++) {
             char op = string.charAt(i);
-            if (yunSuanFuSet.contains(String.valueOf(op))) {
+            if (SymbolSets.OPERATOR_SET.contains(String.valueOf(op))) {
                 //是运算符
                 same.append(op);
-                isyunsuanfu = true;
-            }else if (fenGeFuSet.contains(String.valueOf(op))){
+                isOperator = true;
+            } else if (SymbolSets.SPLITTER_SET.contains(String.valueOf(op))) {
                 stringBuilder.append(" ").append(op).append(" ");
-            }else {
+            } else {
                 //不是运算符
-                if (isyunsuanfu){
+                if (isOperator) {
                     //上一个是运算符
                     stringBuilder.append(" ").append(same.toString()).append(" ");
-                    same.delete(0,same.length());
-                    isyunsuanfu = false;
+                    same.delete(0, same.length());
+                    isOperator = false;
                 }
                 stringBuilder.append(op);
             }
