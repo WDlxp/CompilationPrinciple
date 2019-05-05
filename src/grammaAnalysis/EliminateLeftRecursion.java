912a067284c078a50e395a40a6f588af19846c19
WDL <18959240295@163.com>
2019-05-05 22:19:32 +0800

    抽取出获取新字符的方法

--- src/grammaAnalysis/EliminateLeftRecursion.java
+++ src/grammaAnalysis/EliminateLeftRecursion.java
@@ -11,41 +11,62 @@ import java.util.HashSet;
  */
 public class EliminateLeftRecursion {
     public static void main(String[] args) {
-        HashSet<Character> set = new HashSet<Character>() {{
-            add('A');
-            add('B');
-            add('C');
-            add('D');
-            add('E');
-            add('F');
-            add('G');
-        }};
-        int[] charSet = new int[26];
-        System.out.println(set);
-        for (int i : charSet) {
-            System.out.print(i + ",");
-        }
+    }
 
-        //标记已经使用过的字符
-        for (char ch : set) {
-            charSet[ch - 'A'] = 1;
-        }
-        System.out.println();
-        for (int i : charSet) {
-            System.out.print(i + ",");
-        }
-        System.out.println();
-        //寻找一个未使用的字符
-        int index = 0;
-        for (; index < 26; index++) {
-            if (charSet[index] == 0) {
-                //使用后记得标记
-                charSet[index]=1;
-                set.add((char) (index+'A'));
-                break;
+    /**
+     * 消除左递归（显式）
+     * @param cfg 输入CFG
+     * @return 返回消除左递归后新的CFG
+     */
+    public static ProductSetToCFG.CFG eliminateLeftRecursion(ProductSetToCFG.CFG cfg){
+        //获取非终结符集合
+        HashSet<Character> nonTerminatorSet=cfg.nonTerminatorSet;
+        NewChar.flagUsedChar(nonTerminatorSet);
+        System.out.println(NewChar.getNewChar(nonTerminatorSet));
+        System.out.println(nonTerminatorSet);
+        System.out.println(NewChar.getNewChar(nonTerminatorSet));
+        System.out.println(nonTerminatorSet);
+        return null;
+    }
+
+    /**
+     * 获取新的不重复字符的方法
+     */
+    private static class NewChar{
+        /**
+         *  用于标记字符是否已经使用过0未使用，1已经使用
+         */
+        private static int[] charUsedFlag = new int[26];
+        /**
+         * 需找新字符时使用的下标
+         */
+        private static int index=0;
+        /**
+         * 标记已经使用过的字符
+         * @param nonTerminatorSet 非终态集
+         */
+        public static void flagUsedChar(HashSet<Character> nonTerminatorSet){
+            for (char ch : nonTerminatorSet) {
+                charUsedFlag[ch - 'A'] = 1;
             }
         }
-        System.out.println("找到未使用过的字符：" + (char) ('A' + index));
-        System.out.println(set);
+
+        /**
+         * 获取新的未重复的字符
+         * @param nonTerminatorSet 非终态集
+         * @return 返回新的字符，无则返回’ ‘
+         */
+        public static char getNewChar(HashSet<Character> nonTerminatorSet){
+            for (; index < 26; index++) {
+                if (charUsedFlag[index] == 0) {
+                    //使用后记得标记
+                    charUsedFlag[index]=1;
+                    char newChar=(char) (index+'A');
+                    nonTerminatorSet.add(newChar);
+                    return newChar;
+                }
+            }
+            return ' ';
+        }
     }
 }
