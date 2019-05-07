49e049018bc4246efe35ea1bc20d606758c847a8
WDL <18959240295@163.com>
2019-05-08 02:19:55 +0800

    增加获取First集的代码注释

--- src/grammaAnalysis/FirstAndFollow.java
+++ src/grammaAnalysis/FirstAndFollow.java
@@ -15,8 +15,8 @@ public class FirstAndFollow {
 
 
     public static void main(String[] args) {
-        String product1 = "Aab|Bd";
-        String product2 = "Bcd|Cf";
+        String product1 = "Aab|Be";
+        String product2 = "Bcd|Cf|ε";
         String product3 = "Cε";
         String product4 = "Cab|df";
         ArrayList<String> productSet = new ArrayList<>();
@@ -67,48 +67,87 @@ public class FirstAndFollow {
         for (int i = 0; i < productSet.size(); i++) {
             symbolOfIndex.put(productSet.get(i).left, i);
         }
-
+        //自下而上求First集
+        ProductSetToCFG.Product product;
         for (int index = productSet.size() - 1; index >= 0; index--) {
-            ProductSetToCFG.Product product = productSet.get(index);
+            //获取当前的产生式
+            product = productSet.get(index);
+            //求当前产生式的first集
             product.first = getTotalFirst(productSet, terminatorSet, symbolOfIndex, product.left);
         }
         return cfg;
     }
 
+    /**
+     * 获取产生式总的First集
+     * @param productSet 产生式集合
+     * @param terminatorSet 终态集
+     * @param symbolOfIndex 产生式左侧与对于产生式集合下标的映射
+     * @param left 需要求First集的产生式左侧
+     * @return 放回First集
+     */
     public static HashSet<Character> getTotalFirst(ArrayList<ProductSetToCFG.Product> productSet, HashSet<Character> terminatorSet, HashMap<Character, Integer> symbolOfIndex, char left) {
+        //初始化一个First集
         HashSet<Character> first = new HashSet<>();
+        //获取当前求First集的产生式的下标
         int index = symbolOfIndex.get(left);
+        //获取这个产生式的右侧
         ArrayList<String> rights = productSet.get(index).rights;
+        //遍历产生式右侧求每一项的First集，整合起来即为整个产生式的First集
         for (String right : rights) {
             first.addAll(getFirst(productSet, terminatorSet, symbolOfIndex, left, right));
         }
         return first;
     }
 
+    /**
+     *求产生式右侧一项的First集
+     * @param productSet 产生式集合
+     * @param terminatorSet 终态集
+     * @param symbolOfIndex 产生式左侧与对于产生式集合下标的映射
+     * @param left 需要求First集的产生式左侧
+     * @param right 产生式右侧的某一项
+     * @return 返回一项的First集
+     */
     private static HashSet<Character> getFirst(ArrayList<ProductSetToCFG.Product> productSet, HashSet<Character> terminatorSet, HashMap<Character, Integer> symbolOfIndex, char left, String right) {
         HashSet<Character> first = new HashSet<>();
+        //获取第一个字符
         char firstChar = right.charAt(0);
+        //初始化一个过渡的First集
         HashSet<Character> tempFirst=new HashSet<>();
         //如果第一个字符是非终结符直接返回
         if (terminatorSet.contains(firstChar)) {
             first.add(firstChar);
         }
-        //如果第一个字符为产生式左侧不同则处理
+        //如果第一个字符为非产生式且与产生式左侧不同则处理（与产生式左侧相同不处理）
         else if (firstChar != left) {
+            //获取第一个字符对应产生式集合的位置
             int index = symbolOfIndex.get(firstChar);
+            //如果该非总结符对应产生式的First集非空即说明已经求得该产生式的First集，直接使用即可
             if (productSet.get(index).first != null) {
                 tempFirst.addAll(productSet.get(index).first);
-            } else {
+            }
+            //否则重新求这个非终结符对应的产生式的First集
+            else {
                 tempFirst.addAll(getTotalFirst(productSet, terminatorSet, symbolOfIndex, firstChar));
             }
+            //如果当前仅有一个字符
             if (right.length()==1){
+                //则直接将前面求得的结果作为该项最终结果
                 first.addAll(tempFirst);
-            }else {
+            }
+            //不仅一个字符
+            else {
+                //判断该First集中是否含有ε，如果有
                 if (tempFirst.contains('ε')){
+                    //将该First集去掉ε加到所求项的First集中
                     tempFirst.remove('ε');
                     first.addAll(tempFirst);
+                    //将该字符后面字符的First集加到所求项的First集中
                     first.addAll(getFirst(productSet, terminatorSet, symbolOfIndex, right.substring(1).charAt(0), right.substring(1)));
-                }else {
+                }
+                //不含ε则将该First集直接加到所求项的First集中
+                else {
                     first.addAll(tempFirst);
                 }
             }
