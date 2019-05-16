4f28bb9387cf313740cab491a4f4ccd89fa95537
WDL <18959240295@163.com>
2019-05-17 00:10:32 +0800

    加测试注释

--- src/grammaAnalysis/TestMain.java
+++ src/grammaAnalysis/TestMain.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class TestMain {
     public static void main(String[] args) {
         //测试用例1
-        String product1 = "S(L)|aA";
+        String product1 = "S(L)|aA|a";
         String product2 = "AS|ε";
         String product3 = "LSB";
         String product4 = "B,SB|ε";
@@ -28,27 +28,45 @@ public class TestMain {
 //        productSet.add(product2);
 //        productSet.add(product3);
 //        productSet.add(product4);
+
+        //将产生式转为CFG
         ProductSetToCFG.CFGResult pToCfgResult = ProductSetToCFG.pToCFG(productSet);
 //        cfg=pToCFG(null);
+        //判断转CFG过程是否出错
         if (pToCfgResult.getsError() == 0) {
+            System.out.println("产生式转CFG：");
             ProductSetToCFG.showCFG(pToCfgResult.getCfg());
+            //消除左递归
             ProductSetToCFG.CFGResult eliminateLeftRecursionCfgResult = EliminateLeftRecursion.eliminateLeftRecursion(pToCfgResult.getCfg());
             if (eliminateLeftRecursionCfgResult.getsError() == 0) {
+                System.out.println("\n消除左递归：");
                 ProductSetToCFG.showCFG(eliminateLeftRecursionCfgResult.getCfg());
-                ProductSetToCFG.CFGResult firstAndFollowCfgResult = FirstAndFollow.getFirstAndFollow(eliminateLeftRecursionCfgResult.getCfg());
-                if (firstAndFollowCfgResult.getsError() == 0) {
-                    ProductSetToCFG.showCFG(firstAndFollowCfgResult.getCfg());
-                    PredictionTable.PreTableResult preTableResult = PredictionTable.predictionTable(firstAndFollowCfgResult.getCfg());
-                    if (preTableResult.getsError() == 0) {
-                        PredictionTable.showPreTable(preTableResult.getPreTable(), preTableResult.getColSymbolSet(), preTableResult.getRowSymbolSet());
-                        String input="a";
-                        System.out.println(PushDownAutomaton.predictiveAnalyzerSolution2(input,preTableResult));
-                    } else if (preTableResult.getsError() == PredictionTable.EXIST_IMPLICIT_LEFT_FACTOR) {
-                        System.out.println("存在隐式左因子，不符合LL(1)文法");
+                //提取左因子
+                ProductSetToCFG.CFGResult extractLeftFactorCfgResult = ExtractLeftFactor.extractLeftFactor(eliminateLeftRecursionCfgResult.getCfg());
+                if (extractLeftFactorCfgResult.getsError()==0){
+                    System.out.println("\n提取左因子：");
+                    ProductSetToCFG.showCFG(extractLeftFactorCfgResult.getCfg());
+                    //获取First集和Follow集
+                    ProductSetToCFG.CFGResult firstAndFollowCfgResult = FirstAndFollow.getFirstAndFollow(eliminateLeftRecursionCfgResult.getCfg());
+                    if (firstAndFollowCfgResult.getsError() == 0) {
+                        System.out.println("\n获取First集和Follow集：");
+                        ProductSetToCFG.showCFG(firstAndFollowCfgResult.getCfg());
+                        //生成预测分析表
+                        PredictionTable.PreTableResult preTableResult = PredictionTable.predictionTable(firstAndFollowCfgResult.getCfg());
+                        if (preTableResult.getsError() == 0) {
+                            PredictionTable.showPreTable(preTableResult.getPreTable(), preTableResult.getColSymbolSet(), preTableResult.getRowSymbolSet());
+                            //需要判断的字符
+                            String input = "a";
+                            //下推自动机输出判断结果
+                            System.out.println(PushDownAutomaton.predictiveAnalyzerSolution2(input, preTableResult));
+                        } else if (preTableResult.getsError() == PredictionTable.EXIST_IMPLICIT_LEFT_FACTOR) {
+                            System.out.println("存在隐式左因子，不符合LL(1)文法");
+                        }
+                    } else if (firstAndFollowCfgResult.getsError() == FirstAndFollow.INTERSECTION_OF_FIRST_AND_FOLLOW_IS_NOT_NULL) {
+                        System.out.println("First集含空时与Follow集存在存在交集，不符合LL(1)文法");
                     }
-                } else if (firstAndFollowCfgResult.getsError() == FirstAndFollow.INTERSECTION_OF_FIRST_AND_FOLLOW_IS_NOT_NULL) {
-                    System.out.println("First集含空时与Follow集存在存在交集，不符合LL(1)文法");
                 }
+
 
             } else if (eliminateLeftRecursionCfgResult.getsError() == EliminateLeftRecursion.UNABLE_TO_ELIMINATE_LEFT_RECURSION) {
                 System.out.println("存在无法消除的左递归，不符合LL(1)文法");
