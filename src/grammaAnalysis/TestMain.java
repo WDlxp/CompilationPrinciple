3c41b68434c4916e1d532ddeb3f2a0b3ebe84e3b
WDL <18959240295@163.com>
2019-06-06 11:12:42 +0800

    文件输入和结果的输出

--- src/grammaAnalysis/TestMain.java
+++ src/grammaAnalysis/TestMain.java
@@ -32,46 +32,78 @@ public class TestMain {
 
         //产生式的集合
         ArrayList<String> productSet = new ArrayList<>();
+        ArrayList<String> words = new ArrayList<>();
         try {
-            //获取文件
-            File file=new File("src/grammaAnalysis/test");
-            FileInputStream productSetInputStream=new FileInputStream(file);
+            //获取ProductSet产生式集合
+            File productSetFile = new File("src/grammaAnalysis/productSet");
+            FileInputStream productSetInputStream = new FileInputStream(productSetFile);
             InputStreamReader productSetReader = new InputStreamReader(productSetInputStream, "UTF-8");
-            BufferedReader productSetBufferedReader=new BufferedReader(productSetReader);
+            BufferedReader productSetBufferedReader = new BufferedReader(productSetReader);
             String productString;
-            while ((productString=productSetBufferedReader.readLine())!=null) {
-                productString=removeSpace(productString);
-                if (productString.length()!=0){
+            while ((productString = productSetBufferedReader.readLine()) != null) {
+                productString = removeSpace(productString);
+                if (productString.length() != 0) {
                     productSet.add(productString);
                 }
-            }
-            for (String product :
-                    productSet) {
-                System.out.println(product);
             }
             productSetBufferedReader.close();
             productSetReader.close();
             // 关闭读取流
             productSetInputStream.close();
             // 关闭输入流,释放系统资源
+
+            //获取单词集合
+            File wordsFile = new File("src/grammaAnalysis/test");
+            FileInputStream wordsInputStream = new FileInputStream(wordsFile);
+            InputStreamReader wordsReader = new InputStreamReader(wordsInputStream, "UTF-8");
+            BufferedReader wordsBufferedReader = new BufferedReader(wordsReader);
+            StringBuilder stringBuilder = new StringBuilder();
+            String wordsLineString;
+            String[] tempWords;
+            while ((wordsLineString = wordsBufferedReader.readLine()) != null) {
+                tempWords = wordsLineString.split(" ");
+                for (String word :
+                        tempWords) {
+                    if (!word.equals("")) {
+                        words.add(word);
+                    }
+                }
+            }
+            System.out.println(words);
+            wordsInputStream.close();
+            wordsReader.close();
+            wordsBufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        grammarAnalysis(productSet);
+        grammarAnalysis(productSet, words);
 
     }
 
-    private static String removeSpace(String product){
-        StringBuilder stringBuilder=new StringBuilder();
-        char[] chars=product.toCharArray();
-        for (char ch:chars){
-            if (ch!=' '){
+    private static String removeSpace(String product) {
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] chars = product.toCharArray();
+        for (char ch : chars) {
+            if (ch != ' ') {
                 stringBuilder.append(ch);
             }
         }
         return stringBuilder.toString();
     }
-    private static void grammarAnalysis(ArrayList<String> productSet){
+
+    /**
+     * 语法分析
+     *
+     * @param productSet 产生式集合
+     * @param words      判断的单词集合
+     */
+    private static void grammarAnalysis(ArrayList<String> productSet, ArrayList<String> words) {
+        if (words.size() == 0) {
+            System.out.println("判断的单词集合为空，请检查单词集合");
+            return;
+        }
+
+
         //将产生式转为CFG
         ProductSetToCFG.CFGResult pToCfgResult = ProductSetToCFG.pToCFG(productSet);
 //        cfg=pToCFG(null);
@@ -86,7 +118,7 @@ public class TestMain {
                 ProductSetToCFG.showCFG(eliminateLeftRecursionCfgResult.getCfg());
                 //提取左因子
                 ProductSetToCFG.CFGResult extractLeftFactorCfgResult = ExtractLeftFactor.extractLeftFactor(eliminateLeftRecursionCfgResult.getCfg());
-                if (extractLeftFactorCfgResult.getsError()==0){
+                if (extractLeftFactorCfgResult.getsError() == 0) {
                     System.out.println("\n提取左因子：");
                     ProductSetToCFG.showCFG(extractLeftFactorCfgResult.getCfg());
                     //获取First集和Follow集
@@ -98,10 +130,23 @@ public class TestMain {
                         PredictionTable.PreTableResult preTableResult = PredictionTable.predictionTable(firstAndFollowCfgResult.getCfg());
                         if (preTableResult.getsError() == 0) {
                             PredictionTable.showPreTable(preTableResult.getPreTable(), preTableResult.getColSymbolSet(), preTableResult.getRowSymbolSet());
-                            //需要判断的字符
-                            String input = "a";
-                            //下推自动机输出判断结果
-                            System.out.println(PushDownAutomaton.predictiveAnalyzerSolution2(input, preTableResult));
+                            // 构建FileOutputStream对象,文件不存在会自动新建
+                            FileOutputStream fop = null;
+                            try {
+                                fop = new FileOutputStream("src/grammaAnalysis/result");
+                                // 构建OutputStreamWriter对象,参数可以指定编码,默认为操作系统默认编码,windows上是gbk
+                                OutputStreamWriter writer = new OutputStreamWriter(fop, "UTF-8");
+                                boolean result;
+                                for (String word :
+                                        words) {
+                                    result = PushDownAutomaton.predictiveAnalyzerSolution2(word, preTableResult);
+                                    System.out.println(word + ":" + result);
+                                    writer.append(word + ":" + result + "\n");
+                                }
+                                writer.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                         } else if (preTableResult.getsError() == PredictionTable.EXIST_IMPLICIT_LEFT_FACTOR) {
                             System.out.println("存在隐式左因子，不符合LL(1)文法");
                         }
