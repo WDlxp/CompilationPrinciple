a690478fe42f3cd803f83c2e81c65b5fbb09cfe2
WDL <18959240295@163.com>
2019-06-06 10:42:36 +0800

    文件输入产生式

--- src/grammaAnalysis/TestMain.java
+++ src/grammaAnalysis/TestMain.java
@@ -1,5 +1,6 @@
 package grammaAnalysis;
 
+import java.io.*;
 import java.util.ArrayList;
 
 
@@ -9,15 +10,15 @@ import java.util.ArrayList;
 public class TestMain {
     public static void main(String[] args) {
         //测试用例1
-        String product1 = "S(L)|aA";
-        String product2 = "AS|ε";
-        String product3 = "LSB";
-        String product4 = "B,SB|ε";
-        ArrayList<String> productSet = new ArrayList<>();
-        productSet.add(product1);
-        productSet.add(product2);
-        productSet.add(product3);
-        productSet.add(product4);
+//        String product1 = "S(L)|aA";
+//        String product2 = "AS|ε";
+//        String product3 = "LSB";
+//        String product4 = "B,SB|ε";
+//        ArrayList<String> productSet = new ArrayList<>();
+//        productSet.add(product1);
+//        productSet.add(product2);
+//        productSet.add(product3);
+//        productSet.add(product4);
 //        测试用例2
 //        String product1 = "ABb|Ac";
 //        String product2 = "BAb|bc|Ac|bC";
@@ -29,6 +30,48 @@ public class TestMain {
 //        productSet.add(product3);
 //        productSet.add(product4);
 
+        //产生式的集合
+        ArrayList<String> productSet = new ArrayList<>();
+        try {
+            //获取文件
+            File file=new File("src/grammaAnalysis/test");
+            FileInputStream productSetInputStream=new FileInputStream(file);
+            InputStreamReader productSetReader = new InputStreamReader(productSetInputStream, "UTF-8");
+            BufferedReader productSetBufferedReader=new BufferedReader(productSetReader);
+            String productString;
+            while ((productString=productSetBufferedReader.readLine())!=null) {
+                productString=removeSpace(productString);
+                if (productString.length()!=0){
+                    productSet.add(productString);
+                }
+            }
+            for (String product :
+                    productSet) {
+                System.out.println(product);
+            }
+            productSetBufferedReader.close();
+            productSetReader.close();
+            // 关闭读取流
+            productSetInputStream.close();
+            // 关闭输入流,释放系统资源
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        grammarAnalysis(productSet);
+
+    }
+
+    private static String removeSpace(String product){
+        StringBuilder stringBuilder=new StringBuilder();
+        char[] chars=product.toCharArray();
+        for (char ch:chars){
+            if (ch!=' '){
+                stringBuilder.append(ch);
+            }
+        }
+        return stringBuilder.toString();
+    }
+    private static void grammarAnalysis(ArrayList<String> productSet){
         //将产生式转为CFG
         ProductSetToCFG.CFGResult pToCfgResult = ProductSetToCFG.pToCFG(productSet);
 //        cfg=pToCFG(null);
@@ -66,8 +109,6 @@ public class TestMain {
                         System.out.println("First集含空时与Follow集存在存在交集，不符合LL(1)文法");
                     }
                 }
-
-
             } else if (eliminateLeftRecursionCfgResult.getsError() == EliminateLeftRecursion.UNABLE_TO_ELIMINATE_LEFT_RECURSION) {
                 System.out.println("存在无法消除的左递归，不符合LL(1)文法");
             } else if (eliminateLeftRecursionCfgResult.getsError() == EliminateLeftRecursion.SYMBOL_OVERFLOW) {
@@ -78,3 +119,4 @@ public class TestMain {
         }
     }
 }
+
